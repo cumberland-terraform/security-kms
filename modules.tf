01ec31d019423648930df98aa9ce92067061d13b
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 13:05:14 -0500

    update module queries

--- modules.tf
+++ modules.tf
@@ -2,12 +2,4 @@ module "platform" {
   source                = "github.com/cumberland-terraform/platform"
 
   platform              = var.platform
-  
-  hydration             = {
-    vpc_query           = false
-    subnets_query       = false
-    public_sg_query     = false
-    private_sg_query    = false
-    eks_ami_query       = false
-  }
 }
